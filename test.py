4a068d2534545530669b42f0e41bb0c874c4a406
danxidageda <2559249495@qq.com>
2024-12-30 20:03:45 +0800

    init: 新文件

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ with open('PIE-Benchmark/mapping_file.json', 'r') as file:
 for key, value in data.items():
     if'mask' in value:
         del value['mask']
-
+#
 # 将处理后的数据写回到原文件（你也可以选择写入到新文件，修改文件名即可）
 with open('PIE-Benchmark/mapping_file_nomask.json', 'w') as file:
     json.dump(data, file, indent=4)
